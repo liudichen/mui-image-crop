1544d030f8f9481c66314bcb4b2cb25fe281ac64
iimm <liudichen@foxmail.com>
2022-04-16 21:40:49 +0800

    description.zh-CN jsdoc检查忽略

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
+/*
+ * @Description:
+ * @Author: 柳涤尘 https://www.iimm.ink
+ * @LastEditors: 柳涤尘 liudichen@foxmail.com
+ * @Date: 2022-04-08 22:30:42
+ * @LastEditTime: 2022-04-16 21:40:22
+ */
 module.exports = {
   extends: 'eslint-config-egg',
   parser: '@babel/eslint-parser',
@@ -43,5 +50,13 @@ module.exports = {
     'arrow-parens': [ 'warn', 'always' ],
     'no-empty': [ 'error', { allowEmptyCatch: true }],
     'prefer-promise-reject-errors': 'off',
+    'jsdoc/check-tag-names': [
+      'warn',
+      {
+        definedTags: [
+          'description.zh-CN',
+        ],
+      },
+    ],
   },
 };
